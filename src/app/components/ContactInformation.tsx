adacb6583a7647930d01c8b6a32cc8ce6495baea
TazmeenM <tazmeen.mohsin@gmail.com>
2025-07-28 19:01:03 -0400

    Edited formatting for the Contact Us footer
    Changed the layout of the contact information so they would be beside each other, and changed the padding of the components.

--- src/app/components/ContactInformation.tsx
+++ src/app/components/ContactInformation.tsx
@@ -10,18 +10,27 @@ const contentStyle: React.CSSProperties = {
   display: 'flex',
   flexDirection: 'column',
   height: '200px',
-  padding: '4px',
+  paddingTop: '6px',
+  paddingLeft: '4px',
   background: '#0d0054',
   color: '#ffffff',
+};
+
+const contactInfoStyle: React.CSSProperties = {
+  display: 'flex',
+  flexDirection: 'row',
+  gap: '10vw',
 };
 
 const ContactInformation: React.FC = () => (
   <>
   <div style = {contentStyle}>
-    <h1 className = "text-[24px]">Contact Us</h1>
-    <p>Email: jewelry.store@gmail.com</p>
-    <p>Phone Number: 123-456-7890</p>
-    <p>Instagram: @jewelry.store</p>
+    <h1 className = "text-[24px] pb-[2vh]">Contact Us</h1>
+    <div style = {contactInfoStyle}>
+        <p>Email: jewelry.store@gmail.com</p>
+        <p>Phone Number: 123-456-7890</p>
+        <p>Instagram: @jewelry.store</p>
+    </div>
   </div>
   </>
 );
